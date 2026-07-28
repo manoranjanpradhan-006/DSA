20420e406c1a04e4a922f3821d986fd43ad42ad7
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-07-28 10:03:15 +0530

    add main method implementation to inbuiltQueue class

--- DataStructures/Queue/inbuiltQueue.java
+++ DataStructures/Queue/inbuiltQueue.java
@@ -1,5 +1,22 @@
 package DataStructures.Queue;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class inbuiltQueue {
 
+    public static void main(String[] args) {
+        Queue<Integer> queue = new LinkedList<>();
+
+        queue.add(10);
+        queue.add(20);
+        queue.add(30);
+        queue.add(40);
+        queue.add(50);
+
+        System.out.println(queue.peek());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+        System.out.println(queue.poll());
+    }
 }
