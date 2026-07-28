d228f5a9c2a4149f8c453a9634539edad1db657d
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-07-28 09:50:48 +0530

    add Main class files for Queue and Stack data structures

---
+++ DataStructures/Stack/inbuiltStack.java
@@ -0,0 +1,26 @@
+package DataStructures.Stack;
+
+import java.util.Stack;
+
+public class inbuiltStack {
+    public static void main(String[] args) {
+
+        Stack<Integer> stack = new Stack<>();
+
+        stack.push(10);
+        stack.push(20);
+        stack.push(30);
+        stack.push(40);
+        stack.push(50);
+        stack.push(60);
+        stack.push(70);
+
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+        System.out.println(stack.pop());
+    }
+}
