7ca6598edc77df74c54c0e38b29e7ae26fbf45b3
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-13 10:06:53 +0530

    Update CostomQueue implementation: fix end pointer and add insert/remove methods

--- DataStructures/Queue/CostomQueue.java
+++ DataStructures/Queue/CostomQueue.java
@@ -2,7 +2,7 @@ package DataStructures.Queue;
 
 public class CostomQueue {
 
-    int ptr = -1;
+    int end = 0;
     private int[] data;
     private static final int DEFAULT_SIZE = 10;
 
@@ -12,5 +12,36 @@ public class CostomQueue {
 
     public CostomQueue(int size) {
         this.data = new int[size];
+    }
+
+    public boolean isFull() {
+        return end == data.length;
+    }
+
+    private boolean isEmpty() {
+        return end == 0;
+    }
+
+    public boolean insert(int item) {
+        if (isFull()) {
+            return false;
+        }
+        data[end++] = item;
+        return true;
+    }
+
+    public int remove() throws Exception {
+        if (isEmpty()) {
+            throw new Exception("Queue is empty");
+        }
+
+        int removed = data[0];
+
+        for (int i = 1; i < end; i++) {
+            data[i - 1] = data[i];
+        }
+        end--;
+
+        return removed;
     }
 }
--- DataStructures/Stack/StackMain.java
+++ DataStructures/Stack/StackMain.java
@@ -5,7 +5,7 @@ public class StackMain {
 
         CostomStack stack = new CostomStack(10);
 
-        // stack.push(10);
+        stack.push(10);
         // stack.push(20);
         // stack.push(30);
         // stack.push(40);
