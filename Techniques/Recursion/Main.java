09e4542ea84cfd418cad20d849696432325f5a5e
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-22 22:56:50 +0530

    Add RecursionExamples class and update Main class output

--- Techniques/Recursion/Main.java
+++ Techniques/Recursion/Main.java
@@ -2,6 +2,6 @@ package Techniques.Recursion;
 
 public class Main {
     public static void main(String[] args) {
-        
+        System.out.println("java");
     }
 }
---
+++ Techniques/Recursion/RecursionExamples.java
@@ -0,0 +1,12 @@
+package Techniques.Recursion;
+
+public class RecursionExamples {                                             
+    public static void main(String[] args) {
+        System.out.println("hii");
+        System.out.println("chinu");
+    }
+
+    public void display (){
+        System.out.println("I am printinh my self.");
+    }
+}
