0fece6e295154c90aabc0ed73f12c08876bca7e8
manoranjanpradhan-006 <manoranjanpradhan15559@gmail.com>
2026-08-23 15:37:25 +0530

    Remove main method and display method from RecursionExamples class

--- Techniques/Recursion/RecursionExamples.java
+++ Techniques/Recursion/RecursionExamples.java
@@ -1,12 +1,5 @@
 package Techniques.Recursion;
 
 public class RecursionExamples {                                             
-    public static void main(String[] args) {
-        System.out.println("hii");
-        System.out.println("chinu");
-    }
-
-    public void display (){
-        System.out.println("I am printinh my self.");
-    }
+    
 }
